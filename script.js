76f7adb31ca736db45e584af08a6052d3a095b1d
usernamen7 <n7username7@gmail.com>
2017-07-25 15:47:05 +0300

    script add

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 $(document).ready(() => {
-  $('#myModal2 button').on('click', () => {
-    $('.modal').modal('hide');
-  })
+    $('#myModal2 button').on('click', () => {
+        $('.modal').modal('hide');
+    });
+    $("#test").click(() => {
+        sendInvit();
+    });
 })
+
+function sendInvit(index) {
+    let message = 'Hello guys!';
+    let button  = '.connect';
+    $(button).trigger('click');
+    console.log('first modal 1/4');
+
+    setTimeout(
+        () => {
+          if (!message) {
+              $('.send').trigger('click');
+              console.log('empty message 0/4');
+          } else if (message.length > 0) {
+                $('.add-note').trigger('click');
+                console.log('second modal 2/4');
+
+                setTimeout(
+                    () => {
+                        $('.paste').focus();
+                        $('.paste').val(message);
+                        console.log('message pasted 3/4');
+
+                        setTimeout(
+                            () => {
+                                $('.send').trigger('click');
+                                console.log('message sended 4/4');
+                            }, 900
+                        );
+
+                    }, 1615
+                );
+
+            }
+
+        }, 1800);
+}
